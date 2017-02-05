68a3528da4b2459b935226327a697c8483569dac
Olemis Lang <olemis+work@gmail.com>
2017-02-05 09:06:44 -0500

    [tracks] - Modal dialog for track details

---
+++ assets/js/tracks.js
@@ -0,0 +1,61 @@
+
+var app = angular.module('scipyla_tracks', []);
+
+var tracks = {
+  bio: {
+    title_es: 'Bioinformática',
+    title_en: 'Bioinformatics',
+    icon_class: 'icon-36'
+  },
+  physics: {
+    title_es: 'Física',
+    title_en: 'Physics',
+    icon_class: 'icon-37'
+  },
+  elect: {
+    title_es: 'Ingeniería Eléctrica y Automática',
+    title_en: 'Electrical Engineering and Process Automation',
+    icon_class: 'icon-40'
+  },
+  econ: {
+    title_es: 'Economía e Investigación de Operaciones',
+    title_en: 'Economics and Operations Research',
+    icon_class: 'icon-38'
+  },
+  tech: {
+    title_es: 'Tecnologías de Cómputo Emergentes',
+    title_en: 'Emerging Computing Technologies',
+    icon_class: 'icon-39'
+  },
+  earth: {
+    title_es: 'Ciencias de la Tierra',
+    title_en: 'Earth Science',
+    icon_class: 'icon-41'
+  },
+  social: {
+    title_es: 'Ciencias Sociales y Humanidades',
+    title_en: 'Social Science and Humanities',
+    icon_class: 'icon-42'
+  }
+}
+
+app.controller('TracksCtl', function($scope) {
+  var active = window.location.hash.substr(1);
+  
+  function set_active(track_id) {
+    var track = $scope.track = tracks[track_id];
+    // TODO: Translations
+    $scope.active = track_id;
+  }
+
+  $('#track-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var track_id = button.data('track');
+    set_active(track_id);
+  });
+
+  set_active(active);
+});
+
+
+
