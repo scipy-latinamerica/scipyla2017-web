a9b3c48079c445a3a01ac618eda546dc71ded169
Olemis Lang <olemis@gmail.com>
2017-03-11 16:00:38 -0500

    [community] - Generate Ambassadors list

--- assets/js/tracks.js
+++ assets/js/tracks.js
@@ -45,45 +45,46 @@
   }
 
   var app = angular.module('scipyla.tracks', ['ngSanitize', 'mdMarkdownIt']);
-  
-  app.controller('TracksCtl', ['$scope', function($scope) {
-    var active = (window.location.hash.match(/^#\/?(.*)/) || ['', ''])[1];
 
-    function getlang() {
-      // TODO: Current language for translations
-      return 'es';
-    }
-  
-    function set_active(track_id) {
-      var track = $scope.track = tracks[track_id];
-      // TODO: Translations
-      $scope.active = track_id;
-      if (track.desc === undefined) {
-        var lang_id = getlang();
-        track.file = '../../tracks/' + track_id + '.' + lang_id + '.md';
+  app.controller('TracksCtl', ['$scope',
+    function($scope) {
+      var active = (window.location.hash.match(/^#\/?(.*)/) || ['', ''])[1];
+
+      function getlang() {
+        // TODO: Current language for translations
+        return 'es';
       }
-    }
-  
-    $('#track-modal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget);
-      var track_id = button.data('track') || $scope.active;
-      $scope.$apply(function() { set_active(track_id) });
-    });
-  
-    if (tracks[active] !== undefined) {
-      $scope.active = active;
-      setTimeout(function () {
-          $('#track-modal').modal()
-        }, 2000);
-    }
 
-    $scope.move = function(dx) {
-      var index = (tracks_lookup.indexOf($scope.active[1]) + dx) % 7,
-        track_id = tracks_order[index];
-      set_active(track_id);
-    }
+      function set_active(track_id) {
+        var track = $scope.track = tracks[track_id];
+        // TODO: Translations
+        $scope.active = track_id;
+        if (track.desc === undefined) {
+          var lang_id = getlang();
+          track.file = '../../tracks/' + track_id + '.' + lang_id + '.md';
+        }
+      }
+
+      $('#track-modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var track_id = button.data('track') || $scope.active;
+        $scope.$apply(function() { set_active(track_id) });
+      });
+
+      if (tracks[active] !== undefined) {
+        $scope.active = active;
+        setTimeout(function () {
+            $('#track-modal').modal()
+          }, 2000);
+      }
+
+      $scope.move = function(dx) {
+        var index = (tracks_lookup.indexOf($scope.active[1]) + dx) % 7,
+          track_id = tracks_order[index];
+        set_active(track_id);
+      }
 
   }])
  })(); 
-  
-  
+
+
