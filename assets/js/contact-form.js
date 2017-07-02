c85649b11704ff51fb0ee022f695934c12e29376
Olemis Lang <olemis+work@gmail.com>
2017-07-01 20:17:23 -0400

    [home] - Contact form powered by formspree.io

---
+++ assets/js/contact-form.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('#contact-form').submit(function(event) {
+    event.preventDefault()
+    var data = $(this).serialize()
+    $('#contact-form input').each(function() {
+      if (!$.inArray($(this).attr('name'), ['name', 'email', 'subject', 'message']) && !$(this).val()) {
+        $(this).parent().addClass('has-error')
+        $('#contact-form-errors').removeClass('alert-success')
+        $('#contact-form-errors').addClass('alert-danger')
+        $('#contact-form-errors').html('<strong>* CAMPOS REQUERIDOS.</strong> Por favor verifique campos incompletos o incorrectos.')
+      }
+      else {
+        $.ajax({
+          url: 'https://formspree.io/contact@cuban.tech',
+          method: 'POST',
+          data: data,
+          dataType: 'json'
+        })
+          .done(function() {
+            $('#contact-form-errors').removeClass('alert-danger')
+            $('#contact-form-errors').addClass('alert-success')
+            $('#contact-form-errors').html('<strong><span class="glyphicon glyphicon-envelope"></span> ENVIADO CORRECTAMENTE.</strong> Su mensaje ha llegado con &eacute;xito a su destino.')
+          })
+          .fail(function() {
+            $('#contact-form-errors').removeClass('alert-success')
+            $('#contact-form-errors').addClass('alert-danger')
+            $('#contact-form-errors').html('<strong>ERROR!.</strong> Ocurri&oacute; un error al enviar su mensaje, por favor, intentelo de nuevo.')
+          })
+          .always(function() {
+            console.log("complete")
+          });
+      }
+    });
+  });
+});
+
