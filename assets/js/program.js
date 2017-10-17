c6c478e58e7de083bdb55fd5bc40507b7975fc91
Olemis Lang <olemis+work@gmail.com>
2017-10-17 00:43:03 -0400

    [program] refs #18 - Activity stats. Shadows in speakers avatar

--- assets/js/program.js
+++ assets/js/program.js
@@ -20,7 +20,21 @@
       $scope.speakers = window.speakers;
       $scope.getAuthor = function(id) { return speakers_index[id] }
       $scope.activities = window.activities;
+      $scope.stats = (function() {
+        var _stats = {}
+        for (var i = 0; i < window.activities.length; ++i) {
+          var act = activities[i]
+          if (act['id']) {
+            var idx = act.talk_format.indexOf(' (')
+            var label = (idx == -1)? act.talk_format : act.talk_format.slice(0, idx)
+            _stats[label] = (_stats[label] || 0) + 1
+          }
+        }
+        var stats = []
+        for (label in _stats)
+          stats.push([label, _stats[label]])
+        return stats
+      })()
   }])
- })(); 
-
+})()
 
